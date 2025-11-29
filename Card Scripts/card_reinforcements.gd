86d307d8e8a738a00a11a7332fac3072fd051cf7
Jusbusy <justinmorgan@g.ucla.edu>
2025-11-28 22:56:35 -0800

    Fixed issue with attack forms of duplicated units
    Cards which duplicate units (Divide and Reinforcements) now set the attack form of their duplicated properly.

--- Card Scripts/card_reinforcements.gd
+++ Card Scripts/card_reinforcements.gd
@@ -23,4 +23,5 @@ func do_effect():
 	var new_unit_instance = unit.duplicate()
 	Global.get_tree().root.add_child(new_unit_instance)
 	new_unit_instance.init(Vector2i(0, unit_pos.y))
+	new_unit_instance.attack_form = unit.attack_form
 	new_unit_instance.health = unit.max_health
