bf7098d2625a56d9faacf282b965eedee2f48662
Jusbusy <justinmorgan@g.ucla.edu>
2025-11-28 21:29:00 -0800

    Added reinforcements card
    Added a new card "Reinforcements" which spawns a copy of a unit at the beginning of the lane at full health.

---
+++ Card Scripts/card_reinforcements.gd
@@ -0,0 +1,26 @@
+class_name CardReinforcements extends Card
+
+func get_icon():
+	return load("res://UI/Icons/Card_Divide.png")
+func get_name():
+	return "Reinforcements"
+
+func get_cost():
+	return 1
+func get_selection_rules():
+	return [
+		{"flags" : [SLCT_FRIEND]}, 
+	]
+
+func do_effect():
+	var unit_pos = _curr_selections[0]
+	
+	var unit = Global.grid[unit_pos.x][unit_pos.y]
+	
+	if Global.grid[0][unit_pos.y]:
+		return
+	
+	var new_unit_instance = unit.duplicate()
+	Global.get_tree().root.add_child(new_unit_instance)
+	new_unit_instance.init(Vector2i(0, unit_pos.y))
+	new_unit_instance.health = unit.max_health
