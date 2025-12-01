ae04afa19da8f210269638dd1c2fcaaab07c658e
Jusbusy <justinmorgan@g.ucla.edu>
2025-11-30 21:25:05 -0800

    Added card explode

---
+++ Card Scripts/card_explode.gd
@@ -0,0 +1,24 @@
+class_name CardExplode extends Card
+
+func get_icon():
+	return load("res://UI/Icons/Card_Divide.png")
+func get_name():
+	return "Explode"
+
+func get_cost():
+	return 2
+func get_selection_rules():
+	return [
+		{"flags" : [SLCT_FRIEND]}
+	]
+
+func do_effect():
+	var unit_pos = _curr_selections[0]
+	
+	var unit = Global.grid[unit_pos.x][unit_pos.y]
+	var unit_health = unit.health
+	for x in range(Global.grid_size.x):
+		var elem = Global.grid[x][unit_pos.y]
+		if elem && elem.enemy:
+			elem.health -= unit_health
+	unit.health -= unit_health
